752b9b559101c4084cff29db5cc075153c51b031
Andrew Bratcher <37027181+tanksareforcowards@users.noreply.github.com>
2020-05-23 21:58:03 -0400

    making progress

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //Dependencies 
-
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const fs = require('fs');
+const axios = require("axios");
+const path = require("path");
 
 // MySQL Database connection info
 const connection = mysql.createConnection({
@@ -9,14 +11,32 @@ const connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "12345",
-    database: "employeeDB"
+    database: "dbschema"
 })
+
+// Welcome message
+var onStartup = true;
 
 // Initiating mySQL connection:
 connection.connect(function(err) {
-    if (err){
-        throw err
-    }
+    if (err) throw err;
+    console.log("connected to employee database as id " + connection.threadId + "\n");
+    welcomeMenu();
     // add code to begin function
 });
 
+function welcomeMenu() {
+    if(onStartup==true) {console.log("Welcome to the RobCo employee database system.");
+    console.log("Remember that stopping corporate espionage is **everyone's** responsibility.");
+
+    console.log("Loading main menu... Please wait...")
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'main_menu',
+            message: "Select an option below:"
+            choices: []
+        }
+
+    ])
+}
