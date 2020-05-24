dca8bb03247b6675df56c613546a93146a02cd89
Andrew Bratcher <37027181+tanksareforcowards@users.noreply.github.com>
2020-05-23 22:14:42 -0400

    path planning done

--- server.js
+++ server.js
@@ -28,15 +28,66 @@ connection.connect(function(err) {
 function welcomeMenu() {
     if(onStartup==true) {console.log("Welcome to the RobCo employee database system.");
     console.log("Remember that stopping corporate espionage is **everyone's** responsibility.");
-
     console.log("Loading main menu... Please wait...")
     inquirer.prompt([
         {
             type: 'list',
             name: 'main_menu',
-            message: "Select an option below:"
-            choices: []
+            message: "Select an option below:",
+            choices: [
+                'VIEW the database', 
+                'ADD a new employee, job, or department', 
+                'UPDATE an existing employee or manager', 
+                'EXIT the database'
+            ]
         }
 
-    ])
+    ]).then(userChoice => {
+        switch (userChoice.main_menu) {
+            case 'VIEW the database': viewDatabase();
+                break;
+            case 'ADD a new employee, job role, or department' : goToAddMenu();
+                break;
+            case 'UPDATE an existing employee or manager' : goToUpdateMenu();
+                break;
+            case 'EXIT the database' : quitProgram();
+        }
+    });
+    }
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Planned menu pathing:
+// VIEW --> 
+//  +View everything
+//  +View employees
+//  +View roles
+//  +View departments
+
+// ADD -->
+//  +Add new employee
+//  +Add new role
+//  +Add new department
+
+// UPDATE -->
+//  +Update employee role
+//  +Update employee manager
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+function viewDatabase() {
+    // code for viewing the database
+}
+
+function goToAddMenu() {
+
+}
+
+function goToUpdateMenu() {
+
+}
+
+function quitProgram() {
+
 }
